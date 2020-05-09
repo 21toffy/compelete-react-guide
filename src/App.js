c27d1a81452638358b1624f4c6529bc745ba929b
toffy <oketofoke@gmail.com>
2020-05-09 15:56:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+class App extends Component{
+  render(){
+    return React.createElement('div', null, 'h1', 'hi i\'m a react app ');
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>i am a react app</h1>
+//     </div>
+//   );
+// }
+
+export default App;
