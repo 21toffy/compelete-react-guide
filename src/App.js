57cb48a9c9d77e5c90696382012f4275d541cca1
toffy <oketofoke@gmail.com>
2020-05-10 16:30:35 +0100

    if statements in react

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ class App extends Component{
       {name : 'tofunmi', age:'23'},
       {name : 'ayomide', age : '27'},
       {name : 'omoyeni', age : '28'}
-    ]
+              ],
+              
+    showPersons :false
   }
 
   
   switchNameHandler = (newName) =>{
     // alert('was clicked');
-    this .setState( {
+    this.setState( {
         persons : [
           {name : newName, age:'23'},
           {name : 'ayomide', age : '27'},
@@ -36,6 +38,14 @@ class App extends Component{
       )
     }
     
+
+    togglePersonsHandler = () => {
+      //this picks the showPerson list object in the stata array
+      const doesShow = this.state.showPersons;
+      //this sets the state to yhe opposite of the currnt state 
+      this.setState({showPersons: !doesShow})
+
+    }
   render(){
     const style = {
       backgroundColor: 'white',
@@ -49,26 +59,36 @@ class App extends Component{
             <h1>i am a react app</h1>
             <button 
             style = {style}
-            onClick = {this.switchNameHandler.bind(this, 'daddy')}>switch name</button>
-            <p>this is really working!!!</p>
-            <Person 
-            name = {this.state.persons[0].name} 
-            age={this.state.persons[0].age}
-            change = {this.nameChangeHandler}
-            />
-            <Person 
-            name = {this.state.persons[1].name} 
-            age = {this.state.persons[1].age}
-            change = {this.nameChangeHandler}
-            />
-            <Person
-            name = {this.state.persons[2].name} 
-            age = {this.state.persons[2].age}
-            click = {this.switchNameHandler}
-            change = {this.nameChangeHandler}
-            >
-              how do i display this deail
-            </Person>
+            onClick = {this.togglePersonsHandler}>switch name</button>
+            {
+              //this is an if statement that displays some data if the showPersons is true
+              this.state.showPersons === true ?
+              <div>
+              <p>click on the button to hide</p>
+              <Person 
+              name = {this.state.persons[0].name} 
+              age={this.state.persons[0].age}
+              change = {this.nameChangeHandler}
+              />
+              <Person 
+              name = {this.state.persons[1].name} 
+              age = {this.state.persons[1].age}
+              change = {this.nameChangeHandler}
+              />
+              <Person
+              name = {this.state.persons[2].name} 
+              age = {this.state.persons[2].age}
+              click = {this.switchNameHandler}
+              change = {this.nameChangeHandler}
+              >
+                how do i display this deail
+              </Person>
+              
+              </div>
+              //this is the else statement that displays an alternative if the sowPersons state returns as false
+              : 
+              <p>click on the button to reveal</p>
+            }
           </div>
         );
     // return React.createElement('div', {className:'App'}, React.createElement('h1',{className:'App'}  ,'hi i\'m a react app '));
