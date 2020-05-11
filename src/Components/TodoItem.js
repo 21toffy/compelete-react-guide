cd1b53dcdc0c4bd0d96495380514498ae6e8a438
toffy <oketofoke@gmail.com>
2020-05-12 00:14:47 +0100

    added proptype to components

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
 export class TodoItem extends Component {
@@ -9,6 +10,10 @@ export class TodoItem extends Component {
             </div>
         )
     }
+}
+
+TodoItem.propTypes = {
+    myTodos: PropTypes.object.isRequired
 }
 
 export default TodoItem
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,14 +1,21 @@
 import React , { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 
 
 class Todos extends Component {
     render(){
         return this.props.todos.map((myTodos) => (
-            <TodoItem myTodos = {myTodos} />
+            <TodoItem key = {myTodos.id} myTodos = {myTodos} />
         ));
     }
+}
+
+
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired 
 }
 
 export default Todos;
